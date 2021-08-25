12b692e16562710f4642897d1f621eb0297c69b6
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-08-25 22:41:33 +0100

    triangle check

--- Exercism/kotlin/triangle/src/main/kotlin/Triangle.kt
+++ Exercism/kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,6 +1,5 @@
 class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
 
-
     val isEquilateral: Boolean = (a == b && b == c)
     val isIsosceles: Boolean = (a == b || b == c || c == a)
     val isScalene: Boolean = (a != b && b != c)
