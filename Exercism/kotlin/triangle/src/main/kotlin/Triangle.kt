17bd25a79f4c26b4909c21b189d7598a2b84a9b5
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-08-26 18:16:39 +0100

    refactor

--- Exercism/kotlin/triangle/src/main/kotlin/Triangle.kt
+++ Exercism/kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,13 +1,13 @@
 class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
 
     init {
-        if (a == 0 || b == 0 || c == 0) throw IllegalArgumentException()
-        if (a.toDouble() + b.toDouble() < c.toDouble()
+        if ((a == 0 || b == 0 || c == 0)
+                || (a.toDouble() + b.toDouble() < c.toDouble()
             || b.toDouble() + c.toDouble() < a.toDouble()
-            || a.toDouble() + c.toDouble() < b.toDouble()) throw IllegalArgumentException()
-        if (a.toDouble() + b.toDouble() == c.toDouble()
+            || a.toDouble() + c.toDouble() < b.toDouble())
+                || (a.toDouble() + b.toDouble() == c.toDouble()
             || b.toDouble() + c.toDouble() == a.toDouble()
-            || a.toDouble() + c.toDouble() == b.toDouble()) throw IllegalArgumentException()
+            || a.toDouble() + c.toDouble() == b.toDouble())) throw IllegalArgumentException()
     }
 
     val isEquilateral: Boolean = (a == b && b == c)
