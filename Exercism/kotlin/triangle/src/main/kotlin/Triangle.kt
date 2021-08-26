bfa97db6519f6cb52ffda7027ac923a04a4cfcdc
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-08-26 18:07:06 +0100

    add checks for degenerate triangle

--- Exercism/kotlin/triangle/src/main/kotlin/Triangle.kt
+++ Exercism/kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -5,7 +5,11 @@ class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
         if (a.toDouble() + b.toDouble() < c.toDouble()
             || b.toDouble() + c.toDouble() < a.toDouble()
             || a.toDouble() + c.toDouble() < b.toDouble()) throw IllegalArgumentException()
+        if (a.toDouble() + b.toDouble() == c.toDouble()
+            || b.toDouble() + c.toDouble() == a.toDouble()
+            || a.toDouble() + c.toDouble() == b.toDouble()) throw IllegalArgumentException()
     }
+
     val isEquilateral: Boolean = (a == b && b == c)
     val isIsosceles: Boolean = (a == b || b == c || c == a)
     val isScalene: Boolean = (a != b && b != c)
