588f5857e89a04dcf21d2381f4a926034ba76cc0
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-08-26 17:56:15 +0100

    Valid triangle checks

--- Exercism/kotlin/triangle/src/main/kotlin/Triangle.kt
+++ Exercism/kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -2,6 +2,9 @@ class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
 
     init {
         if (a == 0 || b == 0 || c == 0) throw IllegalArgumentException()
+        if (a.toDouble() + b.toDouble() < c.toDouble()
+            || b.toDouble() + c.toDouble() < a.toDouble()
+            || a.toDouble() + c.toDouble() < b.toDouble()) throw IllegalArgumentException()
     }
     val isEquilateral: Boolean = (a == b && b == c)
     val isIsosceles: Boolean = (a == b || b == c || c == a)
