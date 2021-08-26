418774fe35f381f2da0eeec7bfc1d61820fda57a
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-08-26 10:52:13 +0100

    include IllegalArgumentException handling

--- Exercism/kotlin/triangle/src/main/kotlin/Triangle.kt
+++ Exercism/kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,5 +1,8 @@
 class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
 
+    init {
+        if (a == 0 || b == 0 || c == 0) throw IllegalArgumentException()
+    }
     val isEquilateral: Boolean = (a == b && b == c)
     val isIsosceles: Boolean = (a == b || b == c || c == a)
     val isScalene: Boolean = (a != b && b != c)
