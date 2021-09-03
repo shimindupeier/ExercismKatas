9abf8da572765bacc134db3aa9f3c7097398c26a
shimindupeier <87096821+shimindupeier@users.noreply.github.com>
2021-09-03 23:07:40 +0100

    Update LargestSeriesProduct.kt

--- Exercism/kotlin/largest-series-product/src/main/kotlin/LargestSeriesProduct.kt
+++ Exercism/kotlin/largest-series-product/src/main/kotlin/LargestSeriesProduct.kt
@@ -1,21 +1,16 @@
-class Series(input:String, span:Int) {
+ut:String) {
     //TODO: Implement proper constructor
     init{
-        if(input.isEmpty() && span ==0) getLargestProduct(0)
-        else if((input.length <span)
-                ||input.isEmpty()
-                ||input.contains(Regex("[^0-9]"))
-                ||span <0)
-        throw IllegalArgumentException()
+        if (input.contains(Regex("[^0-9]")))
+            throw IllegalArgumentException()
     }
-
     private val inputLongList =input.map { it.toString().toLong() }.toList()
-
-    fun getLargestProduct(span:Int): Long?{
-        if(span ==0) return 1
-
+    fun getLargestProduct(span:Int): Long{
+        if (span == 0) return 1
+        if (span < 0
+            || (inputLongList.size < span)) throw IllegalArgumentException()
         val lspList =inputLongList.windowed(span, 1).toList()
-            .map { i ->i.reduce { acc, i ->acc *i } }
-        return lspList.max()
+            .map { it.reduce { acc, i ->acc *i } }
+        return lspList.max()!!
     }
 }
